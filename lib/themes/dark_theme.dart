f266837295f4b160448549bf43fc5439a855495b
kartarake <kar.prahveen@gmail.com>
2025-07-15 22:50:56 +0530

    AppBar
    + Working AppBar
    + Icons
    + Theme Setup

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+
+  scaffoldBackgroundColor: Color(0xFF000000),
+
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      color: Color(0xFFFFFFFF),
+      fontFamily: "Montserrat",
+      fontSize: 16.0,
+      fontWeight: FontWeight.w400,
+    ),
+  ),
+
+  appBarTheme: AppBarTheme(
+    backgroundColor: Color(0xFF000000),
+  )
+);
